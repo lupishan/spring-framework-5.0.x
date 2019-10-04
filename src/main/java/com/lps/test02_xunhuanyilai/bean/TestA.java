e27114b543068b218195aafa4b08e704bcfd316b
lps <lupishan@126.com>
2019-10-05 02:34:58 +0800

    spring源码分析-init

---
+++ src/main/java/com/lps/test02_xunhuanyilai/bean/TestA.java
@@ -0,0 +1,8 @@
+package com.lps.test02_xunhuanyilai.bean;
+
+public class TestA {
+    private TestB testB;
+    TestA(TestB testB){
+        this.testB = testB;
+    }
+}
---
+++ src/main/java/com/lps/test02_xunhuanyilai/bean/TestB.java
@@ -0,0 +1,8 @@
+package com.lps.test02_xunhuanyilai.bean;
+
+public class TestB {
+    private TestC testC;
+    TestB(TestC testC){
+        this.testC = testC;
+    }
+}
